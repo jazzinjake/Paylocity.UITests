aec1641bcab1416b03f80abb66363a3465c7e9dc
Jacob Rosenblum <jacob@redsolid.com>
2020-10-27 22:51:44 -0700

    Add project files.

---
+++ Paylocity.UITests/PaylocityWebAppShould.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Paylocity.UITests
+{
+    public class PaylocityWebAppShould
+    {
+        private const string LoginUrl = "https://wmxrwq14uc.execute-api.us-east-1.amazonaws.com/Prod/Account/Login";
+        private const string TestUserName = "TestUser34";
+        private const string TestPassword = "|q$Wl9|&Xk_)";
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void LoadLoginPage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                const string loginTitle = "Log In - Paylocity Benefits Dashboard";
+
+                driver.Navigate().GoToUrl(LoginUrl);
+
+                Assert.Equal(LoginUrl, driver.Url);
+                Assert.Equal(loginTitle, driver.Title);
+            }
+        }
+    }
+}
